12df6b5ac5358281ed8659e143bdc5c4f933b653
Swafey Karanja <kswafey@gmail.com>
2025-05-23 17:03:29 +0300

    into the bakend

--- src/app/(nonDashBoard)/landing/HeroSection.tsx
+++ src/app/(nonDashBoard)/landing/HeroSection.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative min-h-screen overflow-hidden">
       <Image
         src="/landing-splash.jpg"
         alt="Rentiful platform background"
@@ -19,12 +21,30 @@ const HeroSection = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="absolute top-1/3 transform -translate-x-1/2 -translate-y-1/2 w-full"
+        className="absolute inset-0 flex items-center justify-center"
       >
-        <div className="max-w-4xl mx-auto px-16 sm:px-12">
+        <div className="max-w-4xl mx-auto px-16 sm:px-12 text-center">
           <h1 className="text-5xl font-bold text-white mb-4">
             Start your journey to finding the perfect place to call home
           </h1>
+          <p className="text-xl text-white mb-8">
+            Explore our wide range of rental properties tailored to meet your lifestyle and needs!
+          </p>
+          <div className="flex justify-center">
+            <Input
+              type="text"
+              value='Search query'
+              onChange={() => {}}
+              placeholder="search by city, neighbourhood or address"
+              className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
+            />
+            <Button
+              onClick={() => {}}
+              className="bg-red-400 text-white text-lg rounded-none rounded-r-xl border-none hover:bg-red-500 h-12"
+            >
+              Search
+            </Button>
+          </div>
         </div>
       </motion.div>
     </div>
--- src/app/(nonDashBoard)/landing/page.tsx
+++ src/app/(nonDashBoard)/landing/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
+import FooterSection from './FooterSection'
 
 const Landing = () => {
   return (
-    <div>
+    <>
       <HeroSection />
-    </div>
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
+    </>
   )
 }
 

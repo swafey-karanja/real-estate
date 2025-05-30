89a1708a0a6653a782061600dc2a323861eb8f37
Swafey Karanja <kswafey@gmail.com>
2025-05-30 16:19:30 +0300

    added authentication using aws cognito

--- real-estate-client/src/app/providers.tsx
+++ real-estate-client/src/app/providers.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
 import React from "react";
+import Auth from "./(auth)/authProvider";
 
 const Providers = ({ children } : { children: React.ReactNode }) => {
     return (
         <StoreProvider>
-            {children}
+           <Authenticator.Provider>
+                <Auth>
+                    {children}
+                </Auth>
+           </Authenticator.Provider>
         </StoreProvider>
     )
 }
